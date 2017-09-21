0000ab8517d42a012290a25c8fb0293577322810
MiyazawaKohei <miyazawa-kohei0214@g.ecc.u-tokyo.ac.jp>
2017-09-21 13:13:01 +0900

    一様ランダムに正規ベクトルを作成(Create_Random_Normarized_Vector)の追加

--- HEAP_ALGORITHM.cpp
+++ HEAP_ALGORITHM.cpp
@@ -1,6 +1,9 @@
+// comple with "-std=c++11"
+
 #include <iostream>
 #include <random>
 #include <vector>
+#include <math.h>
 #include "Eigen/SVD"
 static const int dimention = 3;
 using namespace std;
@@ -27,12 +30,33 @@ void Close_Vectors_inB(vector<double*> B){
 	return;
 }
 
+double* Create_Random_Normarized_Vector(){
+	double Normarized_Vector[3];
+	uniform_real_distribution<double> lambda(-1.0,1.0);
+	random_device rd;
+	std::mt19937 mt(rd());
+	double theta=asin(lambda(mt));
+	uniform_real_distribution<double> _phi(0,2*M_PI);
+	double phi=_phi(mt);
+	Normarized_Vector[0] = cos(theta)*cos(phi);
+	Normarized_Vector[1] = cos(theta)*sin(phi);
+	Normarized_Vector[2] = sin(theta);
+	for(int i=0; i<3;i++){
+		cout<<Normarized_Vector[i]<<endl;
+	}
+	return Normarized_Vector;
+}
+
+
 int main(){
 
 	vector<double*> A,B;
+	int n_a, n_b; //The number of the vectors of A, B
+
 	B=Init_Vectors_inB(5);
 	for(int i=0; i<5;i++){
 		cout<<B[i][0]<<","<<B[i][1]<<","<<B[i][2]<<endl;
 	}
+	Create_Random_Normarized_Vector();
 	Close_Vectors_inB(B);
 }
